6fcf929332c7bf9e5f389d793c3e11bc59151c06
aswin4400 <aswin4400@gmail.com>
2018-10-07 22:31:43 +0530

    hello

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -132,3 +133,4 @@ STATICFILES_DIRS = (
 AUTH_USER_MODEL = 'sis.CustomUser'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
