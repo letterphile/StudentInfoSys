98e45c1f5fe8854a3174545bfe513463bc14d790
aswin4400 <aswin4400@gmail.com>
2018-10-17 07:48:26 +0530

    time to local time

--- sis/templatetags/sis_tags.py
+++ sis/templatetags/sis_tags.py
@@ -1,5 +1,5 @@
 from django import template
-
+from datetime import timedelta
 register = template.Library()
 def user_auth(user,logs):
         return {'user':user,'logs':logs}
@@ -15,3 +15,8 @@ def student_user(user):
     return {'user':user}
 
 register.inclusion_tag('index.html')(student_user)
+@register.simple_tag
+def local_time(time):
+    new_time = time + timedelta(seconds=19800)
+    new_time = str(new_time)[:-13]
+    return new_time
